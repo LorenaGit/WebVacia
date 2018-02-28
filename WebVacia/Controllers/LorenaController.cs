8e6f8e4d9894356e00dbfed2ab5bb5face4e8670
Lorena Carvajal <lorenacarabreu@gmail.com>
2018-02-28 16:30:04 -0500

    mi carga al repo

---
+++ WebVacia/Controllers/LorenaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebVacia.Controllers
+{
+    public class LorenaController : Controller
+    {
+
+        // GET /Lorena/Carvajal
+        public string Carvajal()
+        {
+            string pagina = "";
+
+            pagina += "<html>";
+
+            pagina += "<head>";
+            pagina += "<script>";
+            pagina += "</scrpt>";
+            pagina += "</head>";
+            pagina += "<body>Este es el body";
+            pagina += "</body>";
+            pagina += "</html>";
+
+            return pagina;
+        }
+
+
+    }
+}
---
+++ WebVacia/Controllers/ProductoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebVacia.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+
+
+
+            ViewBag.mensaje = "Hola Mundo";
+
+
+            return View();
+        }
+    }
+}
